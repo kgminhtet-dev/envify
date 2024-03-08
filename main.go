46c3fe1e5353f500da1a95ead82cb65e3ae8a31a
Kaung Min Htet <kgminhtet.dev@gmail.com>
2024-03-08 16:56:06 +0630

    Added reading .env file and returned key-value pairs function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"io"
 	"io/fs"
 	"log"
 	"os"
@@ -29,8 +32,39 @@ func filterDotEnv(files []fs.DirEntry) []fs.DirEntry {
 	return dotEnvFiles
 }
 
+func getKeyValue(line string) (string, string) {
+	pair := strings.Split(line, "=")
+	return pair[0], pair[1]
+}
+
+func readDotEnv(files []fs.DirEntry) map[string]string {
+	result := make(map[string]string) // key-value pair
+	for _, file := range files {
+		envFile, err := os.Open(file.Name())
+		if err != nil {
+			log.Fatalf("can't read file %s, error %s", file.Name(), err)
+		}
+		defer envFile.Close()
+		envReader := bufio.NewReader(envFile)
+		for {
+			line, err := envReader.ReadString('\n')
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					key, value := getKeyValue(line)
+					result[key] = value
+					break
+				}
+				log.Fatalf("error reading file %s, error %s", file.Name(), err)
+			}
+			key, value := getKeyValue(line)
+			result[key] = value
+		}
+	}
+	return result
+}
+
 func main() {
-	for _, file := range filterDotEnv(readCWD()) {
-		log.Println(file.Name())
+	for key, value := range readDotEnv(filterDotEnv(readCWD())) {
+		log.Printf("key: %s, value: %s\n", key, value)
 	}
 }
